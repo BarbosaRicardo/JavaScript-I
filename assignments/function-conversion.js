dc3bc81de1fa303d6c8ba507910c0675322cad0c
Ricardo Barbosa <Bandito@Ricardos-MacBook-Pro-2.local>
2019-05-25 10:26:31 -0700

    Finished

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,27 +4,40 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+const myFunction = () =>{}
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+const anotherFunction = (param) =>{
+    return param;
+}
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+const add = (param1, param2) => {
+    return param1 + param2;
+}
+add(1,2);
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
+const sub = (param1, param2) =>{
+    return param1 - param2;
+}
+sub(1,2)
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
+exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+const triple = exampleArray.map(x => x*3);
+console.log(triple);
